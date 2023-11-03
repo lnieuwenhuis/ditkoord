5d5062634fb3e162ace150a9d953a33d14d342df
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2023-11-03 11:47:45 +0100

    lk

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <main>Ditkoord website</main>;
+  return <main class="flex flex-col bg-gray-900"></main>;
 }
